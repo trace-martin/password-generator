546489d5f129fac57e5135b6e87be55fcc30ee86
trace-martin <trace.martin7817@gmail.com>
2023-11-14 10:25:15 -0700

    added fields for password

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function generatePassword() {
   } else {
     let passwordLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   }
-  let uppserCase = confirm("Would you liek upper case letters?");
-  if(uppserCase == false) {
+  let upperCase = confirm("Would you like upper case letters?");
+  if(upperCase == false) {
     let passwordUpperCase = [];
   } else {
     let passwordUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -30,6 +30,15 @@ function generatePassword() {
   } else {
     let passwordNum = ["0","1","2","3","4","5","6","7","8","9"];
   }
+  let specialChar = confirm("Would you like to include special characters in your password?");
+  if(specialChar == false) {
+    let passwordSpecialChar = [];
+  } else {
+    let passwordSpecialChar = ["!","@","#","$","%","^","&","*","(",")","-"];
+  };
+
+  const passwordPool = passwordLowerCase.concat(passwordUpperCase, passwordNum, passwordSpecialChar);
+  if( lowerCase == false && uppserCase)
 }
 // Write password to the #password input
 function writePassword() {
