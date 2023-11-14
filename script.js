6424e4bcf17d8ea9f2743b57fed22e3865c95b4e
trace-martin <trace.martin7817@gmail.com>
2023-11-14 10:34:27 -0700

    finished project

--- script.js
+++ script.js
@@ -10,35 +10,45 @@ function generatePassword() {
     window.alert("Your password must be between 8 - 128 characters.");
     return results;
   } else {
-    let passwordLength = varLength;
+    var passwordLength = varLength;
   }
   let lowerCase = confirm("Would you like lower case letters?");
   if(lowerCase == false) {
-   let passwordLowerCase = [];
+   var passwordLowerCase = [];
   } else {
-    let passwordLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var passwordLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   }
   let upperCase = confirm("Would you like upper case letters?");
   if(upperCase == false) {
-    let passwordUpperCase = [];
+    var passwordUpperCase = [];
   } else {
-    let passwordUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var passwordUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   } 
   let numChar = confirm("Would you like to include numbers in your password?");
   if(numChar == false) {
-    let passwordNum = [];
+    var passwordNum = [];
   } else {
-    let passwordNum = ["0","1","2","3","4","5","6","7","8","9"];
+    var passwordNum = ["0","1","2","3","4","5","6","7","8","9"];
   }
   let specialChar = confirm("Would you like to include special characters in your password?");
   if(specialChar == false) {
-    let passwordSpecialChar = [];
+    var passwordSpecialChar = [];
   } else {
-    let passwordSpecialChar = ["!","@","#","$","%","^","&","*","(",")","-"];
+    var passwordSpecialChar = ["!","@","#","$","%","^","&","*","(",")","-"];
   };
 
   const passwordPool = passwordLowerCase.concat(passwordUpperCase, passwordNum, passwordSpecialChar);
-  if( lowerCase == false && uppserCase)
+  if( lowerCase == false && upperCase == false && numChar == false && specialChar == false) {
+    window.alert("You must select at least one password criteria");
+    generatePassword();
+  } else {
+    for(var i = 0; i < passwordLength; i++) {
+      results += passwordPool[Math.floor(Math.random() * passwordPool.length)];
+    };
+    console.log(passwordPool);
+    console.log(results);
+    return results;
+  }
 }
 // Write password to the #password input
 function writePassword() {
